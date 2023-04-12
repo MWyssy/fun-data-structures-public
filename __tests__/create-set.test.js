d3c92501a5bec07c1ecbd9e0041000520e4032b3
MWyssy <mike@wyss.co.uk>
2023-04-12 20:12:43 +0100

    add createSet tests and solution

--- __tests__/create-set.test.js
+++ __tests__/create-set.test.js
@@ -52,5 +52,81 @@ describe('createSet tests', () => {
             //Assert
             expect(testSet.storage).toEqual({});
         });
+        test('should have a delete method which removes the element associated to the value, and returns a boolean asserting whether an element was successfully removed or not.', () => {
+            //Arrange
+            const testSet = createSet();
+            const itemToAdd1 = 'Box 1'
+            const itemToAdd2 = 'Box 2'
+            const itemToAdd3 = 'Box 3'
+            const itemToAdd4 = 'Box 4'
+            //Act
+            testSet.add(itemToAdd1).add(itemToAdd2).add(itemToAdd3).add(itemToAdd4);
+            testSet.delete(itemToAdd2);
+            //Assert
+            expect(testSet.storage).toEqual({1: 'Box 1', 2: 'Box 3', 3: 'Box 4'});
+            expect(testSet.delete(itemToAdd1)).toBe(true);
+            expect(testSet.delete(itemToAdd2)).toBe(false);
+        });
+        test('should have a has method which returns a boolean asserting whether an element is present with the given value in the Set object or not.', () => {
+            //Arrange
+            const testSet = createSet();
+            const itemToAdd1 = 'Box 1'
+            const itemToAdd2 = 'Box 2'
+            const itemToAdd3 = 'Box 3'
+            const itemToAdd4 = 'Box 4'
+            //Act
+            testSet.add(itemToAdd1).add(itemToAdd2).add(itemToAdd3).add(itemToAdd4);
+            //Assert
+            expect(testSet.has(itemToAdd2)).toBe(true);
+            //Act
+            testSet.delete(itemToAdd1);
+            //Assert
+            expect(testSet.has(itemToAdd1)).toBe(false);
+        });
+        test('should have a size method which returns the current number of items in the set', () => {
+            //Arrange
+            const testSet = createSet();
+            const itemToAdd1 = 'Box 1'
+            const itemToAdd2 = 'Box 2'
+            const itemToAdd3 = 'Box 3'
+            const itemToAdd4 = 'Box 4'
+            //Act
+            testSet.add(itemToAdd1).add(itemToAdd2).add(itemToAdd3).add(itemToAdd4);
+            //Assert
+            expect(testSet.size()).toBe(4);
+            //Act
+            testSet.delete(itemToAdd1);
+            //Assert
+            expect(testSet.size()).toBe(3);
+        });
+        test('should have a union method which takes a set as an argument, and adds all of the new items to the existing set from the st passed as an argument', () => {
+            //Arrange
+            const testSet = createSet();
+            const itemToAdd1 = 'Box 1'
+            const itemToAdd2 = 'Box 2'
+            const itemToAdd3 = 'Box 3'
+            const itemToAdd4 = 'Box 4'
+            const itemToAdd5 = 'Box 5'
+            const itemToAdd6 = 'Box 6'
+            const itemToAdd7 = 'Box 7'
+            const setToMerge = {
+                1: itemToAdd4,
+                2: itemToAdd5,
+                3: itemToAdd6,
+                4: itemToAdd7
+            };
+            //Act
+            testSet.add(itemToAdd1).add(itemToAdd2).add(itemToAdd3).add(itemToAdd4);
+            //Assert
+            expect(testSet.union(setToMerge)).toEqual({
+                1: itemToAdd1,
+                2: itemToAdd2,
+                3: itemToAdd3,
+                4: itemToAdd4,
+                5: itemToAdd5,
+                6: itemToAdd6,
+                7: itemToAdd7
+            });
+        });
     });
 });
--- create-set.js
+++ create-set.js
@@ -1,11 +1,11 @@
 //METHODS
-function getQuantity() {
+function size() {
     return Object.keys(this.storage).length;
 }
 
 function add(itemToAdd) {
-    if (!Object.values(this.storage).includes(itemToAdd)) {
-        this.storage[this.getQuantity() + 1] = itemToAdd;
+    if (!this.has(itemToAdd)) {
+        this.storage[this.size() + 1] = itemToAdd;
     }
     return this;
 };
@@ -15,7 +15,36 @@ function clear() {
         delete this.storage[item];
     }
     return this;
-}
+};
+
+function deleteItem(itemToDelete) {
+    const currentQuantity = this.size()
+    const keyToDelete = Object.keys(this.storage).filter((item) => this.storage[item] === itemToDelete)[0];
+    const isItemPresent = this.has(itemToDelete);
+
+    delete this.storage[keyToDelete];
+
+    for (let i = (Number(keyToDelete) + 1); i <= currentQuantity; i++) {
+        console.log(this.storage[i - 1], this.storage[i])
+        this.storage[i - 1] = this.storage[i];
+        delete this.storage[i];
+    };
+
+    if (isItemPresent) return true;
+    return false;
+};
+
+function has(item) {
+    if (Object.values(this.storage).includes(item)) return true;
+    return false;
+};
+
+function union(setToMerge) {
+    for (let item in setToMerge) {
+        this.add(setToMerge[item])
+    }
+    return this.storage;
+};
 
 
 //FACTORY FUNCTION
@@ -26,8 +55,11 @@ function createSet() {
 
     //Adding the Methods
     set.add = add;
-    set.getQuantity = getQuantity;
+    set.size = size;
     set.clear = clear;
+    set.delete = deleteItem;
+    set.has = has;
+    set.union = union;
 
     return set;
 };
